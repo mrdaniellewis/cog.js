e1e8a6eb6287438e1130d4d5fdba4a4b126b4cd3
Daniel Lewis <github@mrdaniellewis.co.uk>
2016-01-07 22:25:53 +0000

    Simplify test layout

--- test/test.js
+++ test/test.js
@@ -1,3 +1,34 @@
+/**
+ *	Test the main framework
+ *	This must be run as part of test.htm which contains
+ *	some test setup.
+ */
+
+describe( 'head.js', function() {
+
+	describe( '<html> classname', function() {
+
+		it( 'should contain "js"', function() {
+
+			var html = document.documentElement;
+			var hasJsClass = html.classList.contains('js');
+			
+			expect( hasJsClass ).toBe( true );
+
+		} );
+
+		it( 'should not contain "no-js"', function() {
+
+			var html = document.documentElement;
+			var hasNoJsClass = html.classList.contains('no-js');
+
+			expect( hasNoJsClass ).toBe( false );
+
+		} );
+	} );
+
+} );
+
 describe( 'cog.js', function() {
 
 	describe( 'window.cog', function() {
